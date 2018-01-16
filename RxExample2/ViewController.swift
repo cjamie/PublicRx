d049d52b941da03da2805d55492bcf54095a441c
Jamie Chu <huanjamierose@gmail.com>
2018-01-16 07:28:54 -0500

    Initial Commit

---
+++ RxExample2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxExample2
+//
+//  Created by Admin on 1/16/18.
+//  Copyright © 2018 Jamie Chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
