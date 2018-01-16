8283ffdd86ba733c101f12e44e4974dca3008ff4
Jamie Chu <huanjamierose@gmail.com>
2018-01-16 10:19:53 -0500

    good template for representing obj in tableview

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'RxExample2' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RxExample2
+
+  target 'RxExample2Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+	pod 'RxSwift'
+	pod 'RxCocoa'
+	pod 'RxDataSources'
+end
--- RxExample2/ViewController.swift
+++ RxExample2/ViewController.swift
@@ -7,19 +7,76 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    let disposeBag = DisposeBag()
+    let CreditDescriptionTitle = ["Card Name", "Product Type", "Brand Name", "Category Tags", "Marketing", "Credit Rating", "Active From", "Benifits", "Fraud Coverage", "Late Payment Fee", "Cash Advance", "Minimum Deposit"]
+    
+    let dataSource =  RxTableViewSectionedReloadDataSource<SectionModel<String, String>>(
+        configureCell: { (_, tv, indexPath, element) in
+            
+            let cell = tv.dequeueReusableCell(withIdentifier: "Cell")!
+            cell.textLabel?.text = element
+//            cell.textLabel?.text?.append(indexPath.row)
+            return cell
+    },
+        titleForHeaderInSection: { dataSource, sectionIndex in
+            return dataSource[sectionIndex].model
+    }
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("VDL")
+        
+        setupTableRx()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func setupTableRx(){
+        let dataSource = self.dataSource
+        
+        let items = Observable.just([
+            SectionModel(model: CreditDescriptionTitle[0], items: [ //model firstSection is my section title
+                "Capital One QUicksilver Cash", //data to display which is a double
+                "nothing",
+                "You qualify"
+                ]),
+            SectionModel(model: CreditDescriptionTitle[1], items: [
+                "Consumer Card",
+                "in reverse0",
+                "Something arb"
+                ]),
+            SectionModel(model: CreditDescriptionTitle[2], items: [
+                "anything",
+                "can",
+                "happen"
+                ])
+            ])
+        
+        
+        items
+            .bind(to: tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+        
+        
+        //for selection (we have no use for it)
+        tableView.rx
+            .itemSelected
+            .map { indexPath in
+                return (indexPath, dataSource[indexPath])
+            }
+            .subscribe(onNext: { tuple in
+                print("pair indexPath:\(tuple.0) data:\(tuple.1)")
+            })
+            .disposed(by: disposeBag)
+        
     }
-
 
 }
+
 
